3ea3685f2c9d39f60656d2b90deab540cac060e7
Kareem Essam <krkrmanstormy@gmail.com>
2024-06-11 18:46:02 +0300

    Issuing the first two tasks

---
+++ 01_Introduction/Task1.py
@@ -0,0 +1,4 @@
+print("Full Name: Kareem Essam")
+print('Birth: 6/4/1999')
+print('Faculty: Assiut')
+print('E-mail: kareemessam153@gmail.com')
---
+++ 01_Introduction/Task2.py
@@ -0,0 +1,8 @@
+def printDifferentArthimaticOp(x, y):
+    print("sum = " + str(x + y))
+    print("diff = " + str(x - y))
+    print("multi = " + str(x * y))
+    print("div = " + str(x / y))
+
+
+printDifferentArthimaticOp(10, 5)
